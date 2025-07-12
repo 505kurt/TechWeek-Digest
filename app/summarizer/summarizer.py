c5faefb9056a228d373671a71f33b76c2035780a
505kurt <fbmakurt@gmail.com>
2025-07-12 11:27:28 -0400

    feat: cloud ai for summarizer and translator
    Use of CloudFlare Workers AI for summary and translation modules

--- app/summarizer/summarizer.py
+++ app/summarizer/summarizer.py
@@ -1,6 +1,17 @@
-from transformers import pipeline
+import os 
+import requests
+from dotenv import load_dotenv
 
-summarizer_pipeline = pipeline("summarization", model="facebook/bart-large-cnn")
+load_dotenv()
+ACCOUNT_ID = os.getenv("ACCOUNT_ID")
+API_TOKEN = os.getenv("API_TOKEN")
+
+API_BASE_URL = f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def run_summarizer(model, input):
+    response = requests.post(f"{API_BASE_URL}{model}", headers=headers, json=input)
+    return response.json()
 
 def summarize_text(text, max_length=160, min_length=80):
     text = text[:2000]
@@ -11,13 +22,24 @@ def summarize_text(text, max_length=160, min_length=80):
         return "Texto vazio. Não foi possível gerar resumo."
 
     try:
-        result = summarizer_pipeline(
-            text,
-            max_length=max_length,
-            min_length=min_length,
-            do_sample=False
+        summarized = run_summarizer(
+            "@cf/facebook/bart-large-cnn", {
+                "input_text": text,
+                'max_lenght': max_length,
+                'min_lenght': min_length
+            }
         )
-        return result[0]['summary_text']
+        return summarized['result']['summary']
     except Exception as e:
         print(f"[Summarizer Error] {e}")
         return "Erro ao gerar o resumo."
+    
+if __name__ == "__main__":
+    text = """
+    OpenAI is an AI research and deployment company. Our mission is to ensure that artificial general intelligence (AGI)—
+    highly autonomous systems that outperform humans at most economically valuable work—benefits all of humanity.
+    We will build safe and beneficial AGI, or help others achieve this outcome.
+    We commit to use any influence we have over AGI to ensure it is used for the benefit of all,
+    and to avoid enabling uses that harm people or concentrate power unduly.
+        """
+    print(summarize_text(text))
--- app/summarizer/translator.py
+++ app/summarizer/translator.py
@@ -1,3 +1,4 @@
+"""
 from transformers import pipeline
 
 translator_pipeline = pipeline("translation", model="unicamp-dl/translation-en-pt-t5")
@@ -9,3 +10,33 @@ def translate_text(text):
     except Exception as e:
         print(f"[Translator Error] {e}")
         return "Erro ao traduzir o resumo."
+    """
+import os 
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+ACCOUNT_ID = os.getenv("ACCOUNT_ID")
+API_TOKEN = os.getenv("API_TOKEN")
+
+API_BASE_URL = f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def run_translator(model, input):
+    response = requests.post(f"{API_BASE_URL}{model}", headers=headers, json=input)
+    return response.json()
+
+def translate_text(text):
+    try:
+        translated = run_translator('@cf/meta/m2m100-1.2b', {
+            'text': text,
+            'source_lang': 'en',
+            'target_lang': 'pt'
+        })
+        return translated['result']['translated_text']
+    except Exception as e:
+        print(f"[Translator Error] {e}")
+        return "Erro ao traduzir o resumo."
+    
+if __name__ == '__main__':
+    print(translate_text("Hello my little friend."))
